2f4e8548200df4e2004b29ecdb3400df3f078d99
unknown <sauvole75@gmail.com>
2021-10-18 23:18:33 +0700

    add

---
+++ app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ArticleComponent} from "./article/article.component";
+
+const routes: Routes = [
+  {path: "article", component : ArticleComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
